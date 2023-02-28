85738c35f47c7ea504a1dbfb5550b52510d208a0
roberto.boston <roberto.bozzo97@tiscali.it>
2023-02-28 18:52:47 +0100

    btn add posts create

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,8 +2,13 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-12 my-5">
-            <h2>Elenco posts</h2>
+        <div class=" d-flex justify-content-between col-12 my-5">
+            <div>
+                <h2>Elenco posts</h2>
+            </div>
+            <div>
+                <a href="{{route('admin.posts.create')}}" class="btn btn-sm btn-primary">Aggiungi Post</a>
+            </div>
         </div>
         <div class="col-12">
             <table class="table table-striped">
